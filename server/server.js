423b58c959a0833cc96ca99a3465eaf542e1dba4
princeparas16 <princeparas16@gmail.com>
2020-10-16 22:50:51 +0530

    Deploy my react SPA to github pages

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import ReactDOMServer from "react-dom/server";
 
 import App from "../src/App";
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 const router = express.Router();
